5ea74192e863274411a48f5f09073439fe373a4a
Sosthène Guédon <sosthene@nitrokey.com>
2023-05-30 12:31:21 +0200

    Fix clippy warnings

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -55,7 +55,7 @@ fuzz_target!(|data: Input| {
             let view = CommandView::try_from(&buffer[..len]).unwrap();
             if !supports_extended {
                 assert!(view.data().len() <= 255);
-                assert_eq!(view.extended(), false);
+                assert!(!view.extended());
                 // Without extended support, le is truncated to 255 at max, and the response will come with command chaining
                 let command = CommandBuilder::new(class, ins, p1, p2, data, le.min(255));
                 assert_eq!(command, view);
@@ -68,7 +68,7 @@ fuzz_target!(|data: Input| {
             let mut parsed = Command::<4096>::try_from(&buffer[..len]).unwrap();
             if !supports_extended {
                 assert!(parsed.data().len() <= 255);
-                assert_eq!(parsed.extended(), false);
+                assert!(!parsed.extended());
             }
             // Loop with arbitrary buflens forever
             for buflen in repeat(buf_lens.iter().chain([&128])).flatten() {
@@ -79,7 +79,7 @@ fuzz_target!(|data: Input| {
                         let view = CommandView::try_from(&buffer[..len]).unwrap();
                         if !supports_extended {
                             assert!(view.data().len() <= 255);
-                            assert_eq!(view.extended(), false);
+                            assert!(!view.extended());
                         }
                         parsed.extend_from_command_view(view).unwrap();
                         if supports_extended {
@@ -99,7 +99,7 @@ fuzz_target!(|data: Input| {
                         let view = CommandView::try_from(&buffer[..len]).unwrap();
                         if !supports_extended {
                             assert!(view.data().len() <= 255);
-                            assert_eq!(view.extended(), false);
+                            assert!(!view.extended());
                         }
                         parsed.extend_from_command_view(view).unwrap();
                     }
