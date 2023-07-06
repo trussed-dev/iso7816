1a89ebc9139aa21c9f580b49f90715ea88d9d81e
Sosthène Guédon <sosthene@nitrokey.com>
2023-07-06 17:31:57 +0200

    Use DataSource for CommandBuilder compilation

--- src/command/datasource.rs
+++ src/command/datasource.rs
@@ -1,24 +1,59 @@
-/// W is a type parameter of the trait and not the method to make the trait dyn-safe
-#[allow(clippy::len_without_is_empty)]
-pub trait DataSource<W: super::Writer> {
+pub trait DataSource {
+    /// Length of the serialized data
     fn len(&self) -> usize;
+
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
+/// Datasource for APDU serialization
+///
+/// W is a type parameter of the trait and not the method to make the trait dyn-safe
+pub trait DataStream<W: super::Writer>: DataSource {
+    /// Serialize the data to a writer.
+    ///
+    /// The length of the data serialized to the writer must not exceed the value returned by `len`.
     fn to_writer(&self, writer: &mut W) -> Result<(), W::Error>;
 }
 
-impl<W: super::Writer> DataSource<W> for &[u8] {
+impl DataSource for [u8] {
     fn len(&self) -> usize {
         <[u8]>::len(self)
     }
+
+    fn is_empty(&self) -> bool {
+        <[u8]>::is_empty(&self)
+    }
+}
+
+impl<W: super::Writer> DataStream<W> for [u8] {
     fn to_writer(&self, writer: &mut W) -> Result<(), W::Error> {
         writer.write_all(self)
     }
 }
 
-impl<W: super::Writer> DataSource<W> for [&dyn DataSource<W>] {
+impl DataSource for [&dyn DataSource] {
     fn len(&self) -> usize {
         self.iter().map(|item| item.len()).sum()
     }
 
+    fn is_empty(&self) -> bool {
+        self.iter().find(|item| !item.is_empty()).is_some()
+    }
+}
+
+impl<W: super::Writer> DataSource for [&dyn DataStream<W>] {
+    fn len(&self) -> usize {
+        self.iter().map(|item| item.len()).sum()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.iter().find(|item| !item.is_empty()).is_some()
+    }
+}
+
+impl<W: super::Writer> DataStream<W> for [&dyn DataStream<W>] {
     fn to_writer(&self, writer: &mut W) -> Result<(), W::Error> {
         for item in self {
             item.to_writer(writer)?;
